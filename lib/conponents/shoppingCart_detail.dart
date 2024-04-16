278ee9179381c70e47dde6a088843d320deb76cb
lhyeonnit <lhyeonnit@gmail.com>
2024-04-16 12:30:40 +0900

    detail 구조잡기

--- lib/conponents/shoppingCart_detail.dart
+++ lib/conponents/shoppingCart_detail.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ShoppingCartDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildDetailNameAndPrice(),
+            _buildDetailRatingAndReviewCount(),
+            _buildDetailColorOptions(),
+            _buildDetailButton(),
+          ],
+        ),
+      ),
+    );
+  }
+  Widget _buildDetailNameAndPrice(){
+    return SizedBox();
+  }
+  Widget _buildDetailRatingAndReviewCount(){
+    return SizedBox();
+  }
+  Widget _buildDetailColorOptions(){
+    return SizedBox();
+  }
+
+  Widget _buildDetailIcon(IconData mIcon){
+    return SizedBox();
+  }
+
+  Widget _buildDetailButton(){
     return SizedBox();
   }
 }
