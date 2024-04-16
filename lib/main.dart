9aff96ebad3602925027ff93e0bc5f0a00686577
lhyeonnit <lhyeonnit@gmail.com>
2024-04-16 11:19:44 +0900

    main AppBar _buildShoppingCartAppBar()

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,7 +18,6 @@ class MyApp extends StatelessWidget {
 }
 
 class ShoppingCartPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +30,21 @@ class ShoppingCartPage extends StatelessWidget {
       ),
     );
   }
-  AppBar _buildShoppingCartAppBar(){
-    return AppBar();
+
+  AppBar _buildShoppingCartAppBar() {
+    return AppBar(
+      leading: IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.arrow_back),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.shopping_cart),
+        ),
+        SizedBox(width: 16),
+      ],
+      elevation: 0.0,
+    );
   }
 }
