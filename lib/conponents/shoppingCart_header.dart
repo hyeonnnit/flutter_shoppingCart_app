f7738ed38df718cfb03d21aa057494446f5b47b5
lhyeonnit <lhyeonnit@gmail.com>
2024-04-16 11:32:31 +0900

    header 추가

--- lib/conponents/shoppingCart_header.dart
+++ lib/conponents/shoppingCart_header.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shopping_cart_app/constants.dart';
 
 class ShoppingCartHeader extends StatefulWidget {
-
   @override
   _ShoppingCartHeaderState createState() => _ShoppingCartHeaderState();
 }
@@ -15,8 +16,64 @@ class _ShoppingCartHeaderState extends State<ShoppingCartHeader> {
     "assets/p3.jpeg",
     "assets/p4.jpeg",
   ];
+
   @override
-  Widget build(BuildContext context){
-    return SizedBox();
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _buildHeaderPic(),
+        _buildHeaderSelector(),
+      ],
+    );
+  }
+
+  Widget _buildHeaderPic() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: AspectRatio(
+        aspectRatio: 5 / 3,
+        child: Image.asset(
+          selectedPic[selectedId],
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeaderSelector() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30,right: 30,top: 10,bottom: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildHeaderSelectorButton(0, Icons.directions_bike),
+          _buildHeaderSelectorButton(1, Icons.motorcycle),
+          _buildHeaderSelectorButton(2, CupertinoIcons.car_detailed),
+          _buildHeaderSelectorButton(3, CupertinoIcons.airplane),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeaderSelectorButton(int id, IconData mIcon) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(
+        color: id == selectedId ? kAccentColor : kSecondaryColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: IconButton(
+        onPressed: () {
+          setState(() {
+            selectedId = id;
+          });
+        },
+        icon: Icon(
+          mIcon,
+          color: Colors.black,
+        ),
+      ),
+    );
   }
 }
